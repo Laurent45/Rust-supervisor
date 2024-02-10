e6592d476ce710cf2f02bc0b66a85de043dbc85f
Laurent FREDERICK <laurent.frederick@icloud.com>
2024-02-10 15:18:31 +0100

    create parse_config_file function

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
 mod parser;
-use crate::parser::Program;
-
-use std::fs;
 
 fn main() {
-    let config = fs::read_to_string("config.yaml").expect("Impossible to read config file");
-    println!("{config}");
-
-    match serde_yaml::from_str::<Vec<Program>>(&config) {
-        Ok(res) => println!("{res:#?}"),
-        Err(err) => println!("{err:#?}"),
-    }
+    let programs = parser::parse_config_file("config.yaml").expect("Config file parse failed");
+    println!("{programs:#?}");
 }
--- src/parser.rs
+++ src/parser.rs
@@ -3,3 +3,12 @@ mod default;
 mod model;
 
 pub use model::Program;
+
+use std::fs;
+
+pub fn parse_config_file(file: &str) -> Result<Vec<Program>, String> {
+    let content = fs::read_to_string(file)
+        .map_err(|err| format!("Impossible to read config file -> {err}"))?;
+    serde_yaml::from_str::<Vec<Program>>(&content)
+        .map_err(|err| format!("Error while parsing config file -> {err}"))
+}
